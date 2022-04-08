438852267cebcfb7b40fc9e319c51274baa84050
hry2 <79210445+hry2@users.noreply.github.com>
2022-04-08 16:24:27 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,3 +4,10 @@ node default {
     content => 'Ceci est une config puppet qui sera déployée sur la master',
   }
  }
+ 
+ node master.puppet.vm {
+    include role::docker_server
+    puppet.vm.test1
+    puppet.vm.test2
+ 
+ }
